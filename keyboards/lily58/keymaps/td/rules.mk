dad0952ce56b5c7f2dc26be7c641ab0fb0e2d34d
terry duong <tduong10101@gmail.com>
2024-05-26 00:19:22 +1000

    updated layout

--- keyboards/lily58/keymaps/td/rules.mk
+++ keyboards/lily58/keymaps/td/rules.mk
@@ -1,7 +1,7 @@
 LTO_ENABLE = yes            # Link Time Optimization enabled
 BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = no        # Mouse keys
-EXTRAKEY_ENABLE = no        # Audio control and System control
+EXTRAKEY_ENABLE = yes        # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = no            # N-Key Rollover
@@ -10,7 +10,7 @@ AUDIO_ENABLE = no           # Audio output
 RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight.
 SWAP_HANDS_ENABLE = no      # Enable one-hand typing
 OLED_ENABLE = yes           # OLED display
-ENCODE_ENABLE = yes
+ENCODER_ENABLE = yes
 WPM_ENABLE = yes
 CONVERT_TO = rp2040_ce
 # If you want to change the display of OLED, you need to change here
